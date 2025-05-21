f5d2f0de0a0db2eb40da9538fe45b4b16533ab8e
x-hachiroku <gh.x86@yukimura.moe>
2025-05-21 20:32:18 +0800

    link media

---
+++ src/tgsync/core/link_media.py
@@ -0,0 +1,108 @@
+import re
+import os
+from pathlib import Path
+from mimetypes import guess_extension
+
+from tgsync.logger import logger
+from tgsync.db.session import session_generator
+from tgsync.db.entities import MessageEntity, PhotoEntity, DocumentEntity
+
+
+def make_safe_filename(name):
+    invalid_chars = r'[\\/:*?"<>|]'
+    name = re.sub(invalid_chars, '_', name)
+
+    name = name.strip(' .')
+
+    reserved = {
+        'CON', 'PRN', 'AUX', 'NUL',
+        *(f'COM{i}' for i in range(1, 10)),
+        *(f'LPT{i}' for i in range(1, 10)),
+    }
+    if name.upper().split('.')[0] in reserved:
+        name = f'_{name}'
+
+    encoded = name.encode('utf-8')
+    safe_name = encoded[:240] + encoded[-10:]
+    safe_name = encoded.decode('utf-8', errors='ignore')
+
+    return safe_name
+
+
+def link_media():
+    with session_generator() as session:
+        candidates = (
+            session.query(
+                MessageEntity,
+                PhotoEntity.id.label('photo_id'),
+            )
+            .join(PhotoEntity, MessageEntity.photo_id == PhotoEntity.id)
+            .filter(
+                MessageEntity.linked == False,
+                PhotoEntity.saved == True
+            )
+        ).all()
+
+        for msg, photo_id in candidates:
+            chat_dir = Path('/media') / str(msg.chat_id)
+            chat_dir.mkdir(parents=True, exist_ok=True)
+
+            dst = chat_dir / f'{msg.id}_{photo_id}.jpg'
+
+            logger.debug(f'Linking {photo_id} to {dst}')
+            if os.path.exists(Path('/media/photos-by-id') / f'{photo_id}.jpg'):
+                if not os.path.exists(dst):
+                    os.link(Path('/media/photos-by-id') / f'{photo_id}.jpg', dst)
+                else:
+                    logger.warning(f'File {dst} already exists, skipping...')
+            else:
+                logger.warning(f'Previous saved photo {photo_id} is removed, skipping {msg.chat_id}/{msg.id}')
+
+            msg.linked = True
+
+    with session_generator() as session:
+        candidates = (
+            session.query(
+                MessageEntity,
+                DocumentEntity.id.label('document_id'),
+                DocumentEntity.name.label('document_name'),
+                DocumentEntity.type.label('document_type')
+            )
+            .join(DocumentEntity, MessageEntity.document_id == DocumentEntity.id)
+            .filter(
+                MessageEntity.linked == False,
+                DocumentEntity.saved == True
+            )
+        ).all()
+
+        for msg, document_id, document_name, document_type in candidates:
+            chat_dir = Path('/media') / str(msg.chat_id)
+            chat_dir.mkdir(parents=True, exist_ok=True)
+
+            ext = guess_extension(document_type)
+            if ext is None:
+                ext = '.bin'
+            src = Path('/media/documents-by-id') / f'{document_id}{ext}'
+
+            filename = f'{msg.id}'
+            if document_name:
+                filename += f' {document_name}'
+            else:
+                filename += ext
+            filename = make_safe_filename(filename)
+            dst = chat_dir / filename
+
+            logger.debug(f'Linking {src} to {dst}')
+            if os.path.exists(src):
+                if not os.path.exists(dst):
+                    os.link(src, dst)
+                else:
+                    logger.warning(f'File {dst} already exists, skipping...')
+            else:
+                logger.warning(f'Previous saved document {document_id} is removed, skipping {msg.chat_id}/{msg.id}')
+
+            msg.linked = True
+
+
+if __name__ == '__main__':
+    link_media()
