ef25291599d80a2a6ab9d58586eb49137f94a390
x-hachiroku <gh.x86@yukimura.moe>
2025-11-12 13:40:12 +0800

    proxy

--- src/tgsync/core/get_client.py
+++ src/tgsync/core/get_client.py
@@ -1,14 +1,33 @@
 from telethon import TelegramClient
+from urllib.parse import urlparse
 
 from tgsync.config import appdata, config
 from tgsync.logger import logger
 
 
 async def get_client():
+    proxy_url = config['tg'].get('proxy')
+    if proxy_url:
+        components = urlparse(proxy_url)
+        proxy = {
+            'proxy_type': components.scheme,
+            'addr': components.hostname,
+            'port': components.port,
+            'username': components.username,
+            'password': components.password,
+            'rdns': False,
+        }
+        if proxy['proxy_type'][-1] == 'h':
+            proxy['proxy_type'] = proxy['proxy_type'][:-1]
+            proxy['rdns'] = True
+    else:
+        proxy = None
+
     client = TelegramClient(
         appdata / config['tg']['session'],
         config['tg']['api_id'],
         config['tg']['api_hash'],
+        proxy=proxy,
     )
     await client.start()
     logger.info('Logging successful.')
